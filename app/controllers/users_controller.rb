9807be98457aba32c8af31b031cf10f78879cba2
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-02-22 23:21:01 -0600

    updated views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,11 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 
 end

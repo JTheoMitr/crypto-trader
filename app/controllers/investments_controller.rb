e1027b60582d47f15135951a33e0f049027ec828
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-02-22 23:13:15 -0600

    Updated Investments Controller

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,4 +1,5 @@
 class InvestmentsController < ApplicationController
+    before_action :set_investment, :redirect_if_not_owner, only: [:show, :edit, :update, :destroy]
 
 
     def new
@@ -19,25 +20,37 @@ class InvestmentsController < ApplicationController
     end
 
     def show
-      @investment = Investment.find(params[:id])
+      
     end
 
     def edit
+     
     end
 
     def update
+      @investment.update(investment_params)
+      redirect_to investment_path(investment)
+    end
+
+    def destroy
+      @investment.destroy
+      redirect_to investments_path
     end
 
 private
 
-def investment_params
-    params.require(:investment).permit(
-      :amount, 
-      cryptocoin_ids:[], 
-      cryptocoins_attributes: [
-        :name
-      ]
-    )
-end
+  def investment_params
+    params.require(:investment).permit(:amount, :cryptocoin_id, cryptocoin_attributes: [:name])
+  end
+
+  def redirect_if_not_owner
+    if current_user != @investment.user
+      redirect_to user_path(current_user), alert: "Unauthorized; No Access to Foreign Investments"
+    end
+  end
+
+  def set_investment
+    @investment = Investment.find(params[:id])
+  end
 
 end

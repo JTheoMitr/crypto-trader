6213470b4d6481202fa83930b4649877a5f0b721
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-02-26 02:30:24 -0600

    updated flash messages and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
+            flash[:message] = @user.errors.full_messages.join(", ")
             render :new
         end
     end
